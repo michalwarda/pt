40b7db48d3d14698ae93d64bae5ff6abcfbda321
Michał Warda <michal.warda@elpassion.pl>
2016-06-24 11:37:53 +0200

    Add input and dynamically render itemList.

--- HelloWorld.lua
+++ HelloWorld.lua
@@ -5,12 +5,42 @@ UIConfig:SetPoint("CENTER", UIParent, "CENTER");
 UIConfig.title = UIConfig:CreateFontString(nil, "OVERLAY");
 UIConfig.title:SetFontObject("GameFontHighlight");
 UIConfig.title:SetPoint("LEFT", UIConfig.TitleBg, "LEFT", 5, 0);
-UIConfig.title:SetText("MUI Buff Options");
+UIConfig.title:SetText("WoW TodoMVC");
 
-local dom = table.concat({"item1", "item2", "item3"}, "<br/>");
+local items = {
+  {text = "Item 1", checked = true},
+  {text = "Item 2", checked = false},
+};
 
-UIConfig.html1 = CreateFrame('SimpleHTML', nil, UIConfig);
-UIConfig.html1:SetText('<html><body><p>' .. dom .. '</p></body></html>');
-UIConfig.html1:SetSize(100, 100);
-UIConfig.html1:SetPoint("TOPLEFT", UIConfig.Bg, 10, -10);
-UIConfig.html1:SetFont('Fonts\\FRIZQT__.TTF', 11);
+function renderInput()
+  local label = UIConfig:CreateFontString(nil, "OVERLAY");
+  label:SetFontObject("GameFontHighlight");
+  label:SetPoint("TOPLEFT", UIConfig.Bg, "TOPLEFT", 10, -10);
+  label:SetText("What needs to be done?");
+
+  local input = CreateFrame("EditBox", "Input", UIConfig, "InputBoxTemplate");
+  input:SetFontObject("GameFontHighlight")
+  input:SetSize(250, 30);
+  input:SetPoint("TOPLEFT", UIConfig.Bg, "TOPLEFT", 15, -20);
+end
+
+function renderItem(index, item)
+  local frame = CreateFrame("Frame", "Item" .. index, UIConfig);
+  frame:SetSize(250, 30);
+  frame:SetPoint("TOP", UIConfig.Bg, "TOP", 0, -((index - 1) * 30) - 50);
+
+  local button = CreateFrame("CheckButton", nil, frame, "UICheckButtonTemplate");
+  button:SetSize(30, 30);
+  button:SetChecked(item.checked);
+  button:SetPoint("CENTER", frame, "LEFT", 0, 0);
+
+  local text1 = UIConfig:CreateFontString(nil, "OVERLAY");
+  text1:SetFontObject("GameFontHighlight");
+  text1:SetPoint("CENTER", frame, "LEFT", 40, 0);
+  text1:SetText(item.text);
+end
+
+table.foreach(items, renderItem);
+renderInput();
+
+print("Eureka");
